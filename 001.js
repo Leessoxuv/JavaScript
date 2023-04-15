00309b683dafaedc180eb09c10a7f18b35aac322
Leessoxuv <thdud3023@gmail.com>
2023-04-15 14:09:22 +0900

    first commit

---
+++ 001.js
@@ -0,0 +1,8 @@
+function greeting() {
+  a = "hello"
+  b = "Chloe" + 3
+  print(a+b);
+}
+greeting();
+
+
---
+++ 004.js
@@ -0,0 +1,8 @@
+var result = true;
+if (result) console.log('result가 참 입니다. ');
+if(!result)
+    console.log('result의 결과');
+if (result) {
+  console.log('result의 결과');
+  console.log('>> 참 입니다.');
+}
